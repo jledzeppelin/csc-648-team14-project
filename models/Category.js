88114156bc263c576e80dcd857962284e723d953
Anthony Carrasco <acarras4@mail.sfsu.edu>
2018-11-07 22:39:20 -0800

    Took out parent_category_id in Category.js

--- models/Category.js
+++ models/Category.js
@@ -23,14 +23,6 @@ class Category extends BaseModel{
     }
 
 
-    get parent_category_id() {
-        return this._parent_category_id
-    }
-
-    set parent_category_id(value) {
-        this._parent_category_id = value
-    }
-
     constructor(){
         super()
     }
@@ -51,7 +43,7 @@ class Category extends BaseModel{
      */
 
     static getAllCategories (){
-        let sql =  `SELECT ${_category_name} FROM ${this.__TABLE}`
+        let sql =  `SELECT _category_name FROM ${this.__TABLE}`
         let allCategories = super.getMultipleBySQL(Category , sql )
         return allCategories
     }
@@ -59,7 +51,7 @@ class Category extends BaseModel{
     /**
      * @description Convert the result from the DB to a new Post object
      * @param result {object} The result from the Database.
-     * @returns {Cateory} The instantiated Category object
+     * @returns {Category} The instantiated Category object
      * @author Anthony Carrasco acarras4@mail.sfsu.edu
      */
     static objectMapper(result){
@@ -68,21 +60,19 @@ class Category extends BaseModel{
         // Take all the values and put them in the new object
         newCategory.id = result.id
         newCategory.category_name = result._category_name
-        newCategory.parent_category_id = result._parent_category_id
 
         return newCategory
     }
 
     /**
      * @description This is what will be returned when converting the object to JSON.
-     * @returns {{id: *, category_name: *, parent_category_id}}
+     * @returns {{id: *, category_name: *, parent_category_id: *}}
      * @author Anthony Carrasco acarras4@mail.sfsu.edu
      */
     toJSON() {
         return {
             id: this.id,
             category_name : this._category_name,
-            parent_category_id: this._parent_category_id
         }
     }
 
