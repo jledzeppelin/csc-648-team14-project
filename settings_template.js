072e5ef19541b75685d4ed3c546cce30d8f42eaa
Jack Cole <jack-cole@users.noreply.github.com>
2018-12-18 12:13:30 -0800

    Normalized settings file

--- settings_template.js
+++ settings_template.js
@@ -1,13 +1,14 @@
 var settings = {}
 
 settings.web = {}
-settings.web.http_port = process.env.PORT || 8080
+settings.web.http_port = process.env.HTTP_PORT || 8080
+settings.web.https_port = process.env.HTTPS_PORT || 8081
 
 settings.db = {
     host     : "",
-    user     : "",
-    password : "",
-    database : "",
+    user     : '',
+    password : '',
+    database : '',
 }
 
 settings.CAPTCHA_SECRET = ""
